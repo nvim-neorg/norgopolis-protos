3f5c9b1778bd09e57123f61c40ee3845a4fab37d
Vhyrro <vhyrro@gmail.com>
2023-09-07 19:26:39 +0200

    fix: move encode and decode implementations into their respective modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,49 @@
+pub use rmp_serde::{decode, encode};
+
 pub mod module_communication {
     tonic::include_proto!("module_communication");
+
+    use rmp_serde::{decode, encode};
+
+    impl MessagePack {
+        pub fn decode<Target>(self) -> Result<Target, decode::Error>
+        where
+            Target: serde::de::DeserializeOwned,
+        {
+            Ok(rmp_serde::decode::from_slice(self.data.as_slice())?)
+        }
+
+        pub fn encode<T>(target: T) -> Result<Self, encode::Error>
+        where
+            T: serde::Serialize,
+        {
+            Ok(MessagePack {
+                data: rmp_serde::encode::to_vec(&target)?,
+            })
+        }
+    }
 }
 
 pub mod client_communication {
     tonic::include_proto!("communication");
-}
 
-pub use rmp_serde::{decode, encode};
+    use rmp_serde::{decode, encode};
 
-impl module_communication::MessagePack {
-    pub fn decode<Target>(self) -> Result<Target, decode::Error>
-    where
-        Target: serde::de::DeserializeOwned,
-    {
-        Ok(rmp_serde::decode::from_slice(self.data.as_slice())?)
-    }
+    impl MessagePack {
+        pub fn decode<Target>(self) -> Result<Target, decode::Error>
+        where
+            Target: serde::de::DeserializeOwned,
+        {
+            Ok(rmp_serde::decode::from_slice(self.data.as_slice())?)
+        }
 
-    pub fn encode<T>(target: T) -> Result<Self, encode::Error>
-    where
-        T: serde::Serialize,
-    {
-        Ok(module_communication::MessagePack {
-            data: rmp_serde::encode::to_vec(&target)?,
-        })
-    }
-}
-
-impl client_communication::MessagePack {
-    pub fn decode<Target>(self) -> Result<Target, decode::Error>
-    where
-        Target: serde::de::DeserializeOwned,
-    {
-        Ok(rmp_serde::decode::from_slice(self.data.as_slice())?)
-    }
-
-    pub fn encode<T>(target: T) -> Result<client_communication::MessagePack, encode::Error>
-    where
-        T: serde::Serialize,
-    {
-        Ok(client_communication::MessagePack {
-            data: rmp_serde::encode::to_vec(&target)?,
-        })
+        pub fn encode<T>(target: T) -> Result<MessagePack, encode::Error>
+        where
+            T: serde::Serialize,
+        {
+            Ok(MessagePack {
+                data: rmp_serde::encode::to_vec(&target)?,
+            })
+        }
     }
 }
